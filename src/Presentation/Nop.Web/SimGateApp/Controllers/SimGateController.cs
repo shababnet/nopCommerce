4a57fff92c398e47ca603e9530bd0b241cc2640c
Ahmed <ahmed@shababnet.com>
2016-04-20 17:39:00 +0300

    Add SimGate Folders to core data services

---
+++ src/Presentation/Nop.Web/SimGateApp/Controllers/SimGateController.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Mvc;
+using Nop.Web.Framework.Security;
+using Nop.Core;
+
+
+namespace Nop.Web.Controllers
+{
+    public partial class SimGateController : BasePublicController
+    {
+        private readonly IWorkContext _workContext;
+
+
+
+        public SimGateController(IWorkContext workContext)
+        {
+            this._workContext = workContext;
+        }
+
+    }
+}
